bd24a214e4f354a73eccaf50f35a7b64333abc05
pratikmarutest <158421513+pratikmarutest@users.noreply.github.com>
2024-06-29 06:31:39 +0000

    removed loader import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice.js";
-import { Header, Footer, Loader } from "./components";
+import { Header, Footer } from "./components";
 import { Outlet } from "react-router-dom";
 import common from "./common/commonText.jsx";
 
